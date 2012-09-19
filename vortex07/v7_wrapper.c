a5eadfabbf8d2788f1b56c8c9081cc68ef8c7e90
Antoine Neuenschwander <antoine@schoggi.org>
2012-09-19 20:47:21 +0200

    added CRC adjustment function

--- vortex07/v7_wrapper.c
+++ vortex07/v7_wrapper.c
@@ -14,14 +14,16 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "crc32_table.h"
 
-#define CRCPOLY  0xEDB88320
-#define CRCINV   0x5B358FD3 // inverse poly of (x^N) mod CRCPOLY
-//#define INITXOR  0xFFFFFFFF
-#define INITXOR  0x00000000
-//#define FINALXOR 0xFFFFFFFF
-#define FINALXOR 0x00000000
+#define CRCPOLY		0xEDB88320
+#define CRCINV		0x5B358FD3 // inverse poly of (x^N) mod CRCPOLY
+//#define INITXOR	0xFFFFFFFF
+#define INITXOR		0x00000000
+//#define FINALXOR	0xFFFFFFFF
+#define FINALXOR	0x00000000
+#define FIXCRC		0xe1ca95ee
 typedef unsigned int uint32;
 
 /**
@@ -60,15 +62,80 @@ int crc32_tabledriven(unsigned char *buffer, int length, uint32 *crc_table) {
 	return crcreg ^ FINALXOR;
 }
 
+/**
+ * Changes the last 4 bytes of the given buffer so that it afterwards will
+ * compute to the given tcrcreg using the given crc_table
+ *
+ * This function uses the method of the multiplication with (x^N)^-1.
+ */
+void fix_crc_end(unsigned char *buffer, int length, uint32 tcrcreg, uint32 *crc_table) {
+	int i;
+	tcrcreg ^= FINALXOR;
+	
+	// calculate crc except for the last 4 bytes; this is essentially crc32()
+	uint32 crcreg = INITXOR;
+	for (i = 0; i < length - 4; ++i) {
+		crcreg = (crcreg >> 8) ^ crc_table[((crcreg ^ buffer[i]) & 0xFF)];
+	}
+
+	// calculate new content bits
+	// new_content = tcrcreg * CRCINV mod CRCPOLY
+	uint32 new_content = 0;
+	for (i = 0; i < 32; ++i) {
+		// reduce modulo CRCPOLY
+		if (new_content & 1) {
+			new_content = (new_content >> 1) ^ CRCPOLY;
+		} else {
+			new_content >>= 1;
+		}
+		// add CRCINV if corresponding bit of operand is set
+		if (tcrcreg & 1) {
+			new_content ^= CRCINV;
+		}
+		tcrcreg >>= 1;
+	}
+	// finally add old crc
+	new_content ^= crcreg;
+
+	// inject new content
+	for (i = 0; i < 4; ++i) {
+		buffer[length - 4 + i] = (new_content >> i*8) & 0xFF;
+	}
+}
 
 int main(int argc, char* argv[]) {
-//	uint32 crc_table[256];
-	uint32 *crc_table = (uint32 *)crc32_table;
+	if (argc < 2) {
+		printf("usage: %s <arg>\n", argv[0]);
+		return 0;
+	}
+	int length = strlen(argv[1]);
+
+	// generate CRC32 table
+	uint32 crc_table_dynamic[256];
+	make_crc_table(crc_table_dynamic);
+
+	// use reference to static CRC32 table
+	uint32 *crc_table = (uint32*)crc_table_static;
+//	uint32 *crc_table = crc_table_dynamic;
+
+	// compute CRC32
 	int crc32;
+	crc32 = crc32_tabledriven(argv[1], length, crc_table);
+	fprintf(stderr, "Original CRC:\t0x%08X\n", crc32);
 
-//	make_crc_table(crc_table);
-	crc32 = crc32_tabledriven(argv[1], strlen(argv[1]), crc_table);
-	printf("0x%08X\n", crc32);
+	// adjust buffer
+	char* buffer = malloc(length + 4);
+	memcpy(buffer, argv[1], length);
+	fix_crc_end(buffer, length + 4, FIXCRC, crc_table);
+	
+	// recompute CRC32
+	crc32 = crc32_tabledriven(buffer, length + 4, crc_table);
+	fprintf(stderr, "Adjusted CRC:\t0x%08X\n", crc32);
 
+	// output adjustment
+	uint32 adjustment = *((uint32*)(buffer + length));
+	fprintf(stderr, "Adjustment:\t0x%08X\n", adjustment);
+
+	fprintf(stdout, "%s", buffer);
 	return 0;
 }
